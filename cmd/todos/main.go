708706bfc394b0b1e06e69906d98b5115878a4f0
gillchristian <gillchristiang@gmail.com>
2017-04-20 23:31:23 -0300

    Naive print

--- cmd/todos/main.go
+++ cmd/todos/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/caarlos0/spin"
 	"github.com/gillchristian/todos"
 	"github.com/urfave/cli"
 )
@@ -18,14 +16,7 @@ func main() {
 	app.Author = "Christian Gill (gillchristiang@gmail.com)"
 	app.Usage = "This is an todos app written in Go"
 	app.Action = func(c *cli.Context) error {
-		spin := spin.New("\033[36m %s Working...\033[m")
-		spin.Start()
-		err := todos.Foo()
-		spin.Stop()
-		if err != nil {
-			return err
-		}
-		fmt.Println("Done!")
+		todos.PrintTodos()
 		return nil
 	}
 	_ = app.Run(os.Args)
--- todos.go
+++ todos.go
@@ -1,9 +1,30 @@
 package todos
 
-import "time"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os/user"
+)
 
-// Foo is my foo function
-func Foo() error {
-	time.Sleep(2 * time.Second)
-	return nil
+// PrintTodos prints today's to-do file
+func PrintTodos() {
+	path := homeDir() + "/.todos/data/2017/04-20.txt"
+	b, err := ioutil.ReadFile(path) // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	str := string(b) // convert content to a 'string'
+
+	fmt.Println(str) // print the content as a 'string'
+}
+
+// homeDir retrieves the home directory for the current user.
+func homeDir() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return usr.HomeDir
 }
