6e8e885b0f2343576358a842f5cf1bd77a6a6211
gillchristian <gillchristiang@gmail.com>
2017-04-26 10:57:04 -0300

    Small refactor

--- todos.go
+++ todos.go
@@ -27,41 +27,6 @@ func (fs files) Less(i, j int) bool {
 	return fs[i].ModTime().Before(fs[j].ModTime())
 }
 
-func readDir(path string) (files, error) {
-	var todoFs files
-
-	fs, err := ioutil.ReadDir(path)
-
-	if err != nil {
-		return files{}, err
-	}
-
-	for _, f := range fs {
-		if !f.IsDir() {
-
-			todoFs = append(todoFs, f)
-		}
-	}
-
-	sort.Sort(todoFs)
-
-	return todoFs, nil
-}
-
-func sanitizePath(path string) string {
-	if !filepath.IsAbs(path) {
-		home := homeDir()
-		path = strings.Replace(path, "$HOME", home, 1)
-		path = strings.Replace(path, "~", home, 1)
-	}
-
-	return filepath.Clean(path)
-}
-
-func todaysPath(basePath string) string {
-	return sanitizePath(basePath + "/" + todaysFilename())
-}
-
 // PrintTodos prints today's to-do file
 func PrintTodos(basePath string) {
 	path := todaysPath(basePath)
@@ -71,8 +36,14 @@ func PrintTodos(basePath string) {
 	printFile(path)
 }
 
+// createTodayFile creates today's to-do file.
+// TODO section is copied from the last to-do file present.
 func createTodayFile(basePath string) {
-	dirPath := sanitizePath(basePath + "/" + currentYear())
+
+	// TODO: make it work for last year files.
+
+	currentYear := strconv.Itoa(time.Now().Year())
+	dirPath := sanitizePath(basePath + "/" + currentYear)
 	fs, err := readDir(dirPath)
 	if err != nil {
 		log.Fatal(err)
@@ -99,6 +70,7 @@ func createTodayFile(basePath string) {
 	fmt.Printf("Created today's to-do file: %s\n\n", todaysFilename())
 }
 
+// printFile prints a file content to the console.
 func printFile(path string) {
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -114,8 +86,25 @@ func todaysFilename() string {
 	return time.Now().Format("2006/01-02.txt")
 }
 
-func currentYear() string {
-	return strconv.Itoa(time.Now().Year())
+// sanitizePath makes sure a path is absolute and clean.
+// It also replaces $HOME & ~ with the home directory for the current user.
+func sanitizePath(path string) string {
+	if !filepath.IsAbs(path) {
+		home := homeDir()
+		if strings.HasPrefix(path, "$HOME") {
+			path = strings.Replace(path, "$HOME", home, 1)
+		}
+		if strings.HasPrefix(path, "~") {
+			path = strings.Replace(path, "~", home, 1)
+		}
+	}
+
+	return filepath.Clean(path)
+}
+
+// todaysPath generates a sanitized path to what should be today's to-do file.
+func todaysPath(basePath string) string {
+	return sanitizePath(basePath + "/" + todaysFilename())
 }
 
 // exists checks whether a file exists or not.
@@ -124,11 +113,32 @@ func exists(name string) bool {
 	return !os.IsNotExist(err)
 }
 
-// homeDir retrieves the home directory for the current user.
+// homeDir retrieves the home directory path for the current user.
 func homeDir() string {
 	usr, err := user.Current()
 	if err != nil {
 		log.Fatal(err)
 	}
 	return usr.HomeDir
+}
+
+// readDir walks a directory and returns a list of all files sorted DESC by ModTime.
+func readDir(path string) (files, error) {
+	var todoFs files
+
+	fs, err := ioutil.ReadDir(path)
+
+	if err != nil {
+		return files{}, err
+	}
+
+	for _, f := range fs {
+		if !f.IsDir() {
+			todoFs = append(todoFs, f)
+		}
+	}
+
+	sort.Sort(todoFs)
+
+	return todoFs, nil
 }
