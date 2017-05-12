b71c3f478b459946456dc793ddc7bdc477e3b4f9
gillchristian <gillchristiang@gmail.com>
2017-05-11 23:39:20 -0300

    [WIP]

--- todos.go
+++ todos.go
@@ -8,10 +8,81 @@ import (
 	"os/user"
 	"path/filepath"
 	"sort"
-	"strconv"
 	"strings"
 	"time"
+
+	"github.com/fatih/color"
 )
+
+type TodoFile struct {
+	todos   []string
+	accomps []string
+	year    string
+	name    string
+	content string
+}
+
+func (t *TodoFile) Parse() error {
+	if t.content == "" || !strings.Contains(t.content, "TODO") || !strings.Contains(t.content, "Accomplished") {
+		return fmt.Errorf("%T does not have a valid content", t)
+	}
+
+	chunks := strings.Split(t.content, "Accomplished")
+
+	todo := strings.TrimSpace(strings.Replace(chunks[0], "TODO", "", 1))
+	accomp := strings.TrimSpace(chunks[1])
+
+	t.todos = list(todo)
+	t.accomps = list(accomp)
+
+	return nil
+}
+
+func (t TodoFile) Path(base string) string {
+	return sanitizePath(base + "/" + t.year + "/" + t.name)
+}
+
+func (t TodoFile) Print() {
+	red := color.New(color.FgRed, color.Bold)
+	green := color.New(color.FgGreen, color.Bold)
+	if len(t.todos) > 0 {
+		red.Println("TODO")
+
+		for i, t := range t.todos {
+			fmt.Printf("%2d - %v\n", i, t)
+		}
+	}
+
+	if len(t.accomps) > 0 {
+		green.Println("\nAccomplished")
+
+		for i, t := range t.accomps {
+			fmt.Printf("%2d - %v\n", i, t)
+		}
+	}
+
+}
+
+func (t TodoFile) Create() error {
+
+	if err := os.Mkdir("~/.todos/"+t.year, os.ModeDir); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func list(s string) []string {
+	l := strings.Split(s, "\n")
+	r := []string{}
+
+	for _, item := range l {
+		if t := strings.Replace(item, "- ", "", 1); t != "" {
+			r = append(r, t)
+		}
+	}
+
+	return r
+}
 
 type files []os.FileInfo
 
@@ -27,13 +98,58 @@ func (fs files) Less(i, j int) bool {
 	return fs[i].ModTime().Before(fs[j].ModTime())
 }
 
+func Init() error {
+	now := time.Now()
+
+	if err := os.Mkdir("~/.todos", os.ModeDir); err != nil {
+		log.Fatal(err)
+	}
+
+	return TodoFile{
+		year: now.Format("2006"),
+		name: now.Format("01-02.txt"),
+	}.Create()
+}
+
+// AddTodo adds a new to-do to today's file.
+func AddTodo(basePath, todo string) error {
+	path := todaysPath(basePath)
+
+	fmt.Printf("Adding: %v - to: %v\n", todo, path)
+
+	return nil
+}
+
 // PrintTodos prints today's to-do file
 func PrintTodos(basePath string) {
 	path := todaysPath(basePath)
 	if !exists(path) {
 		createTodayFile(basePath)
 	}
-	printFile(path)
+
+	now := time.Now()
+
+	t := TodoFile{
+		content: parseTodoFile(path),
+		year:    now.Format("2006"),
+		name:    now.Format("01-02.txt"),
+	}
+
+	if err := t.Parse(); err != nil {
+		log.Fatal(err)
+	}
+
+	t.Print()
+}
+
+func parseTodoFile(path string) string {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content := string(b)
+	return content
 }
 
 // createTodayFile creates today's to-do file.
@@ -42,7 +158,7 @@ func createTodayFile(basePath string) {
 
 	// TODO: make it work for last year files.
 
-	currentYear := strconv.Itoa(time.Now().Year())
+	currentYear := time.Now().Format("2006")
 	dirPath := sanitizePath(basePath + "/" + currentYear)
 	fs, err := readDir(dirPath)
 	if err != nil {
@@ -83,7 +199,7 @@ func printFile(path string) {
 // getTodaysFilename generates todo's filename for today.
 // Format: "<year>/<month>-<day>.txt"
 func todaysFilename() string {
-	return time.Now().Format("2006/01-02.txt")
+	return time.Now().Format("01-02.txt")
 }
 
 // sanitizePath makes sure a path is absolute and clean.
@@ -104,7 +220,7 @@ func sanitizePath(path string) string {
 
 // todaysPath generates a sanitized path to what should be today's to-do file.
 func todaysPath(basePath string) string {
-	return sanitizePath(basePath + "/" + todaysFilename())
+	return sanitizePath(basePath + "/" + time.Now().Format("2006") + "/" + todaysFilename())
 }
 
 // exists checks whether a file exists or not.
