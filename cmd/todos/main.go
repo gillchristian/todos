3a6796b7afaeec4e5d48f3bf796978f0fd157ba4
gillchristian <gillchristiang@gmail.com>
2017-05-18 18:39:59 -0300

    Init command

--- cmd/todos/main.go
+++ cmd/todos/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/gillchristian/todos"
 	"github.com/urfave/cli"
 )
 
 var version = "master"
+
+var red *color.Color = color.New(color.FgRed, color.Bold)
+var green *color.Color = color.New(color.FgGreen, color.Bold)
 
 func main() {
 	var path string
@@ -25,14 +30,39 @@ func main() {
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:        "dir, d",
-			Value:       "$HOME/.todos",
-			Usage:       "todos directory `PATH`",
+			Value:       "~/.todos",
+			Usage:       "todos directory `PATH`.",
 			Destination: &path,
 		},
 	}
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "init",
+			Usage: "initializes the TODO's directory and creates the first to-do file",
+			Action: func(c *cli.Context) error {
+				var dir string
+
+				if c.NArg() > 0 {
+					dir = c.Args()[0]
+				} else {
+					dir = c.GlobalString("dir")
+					fmt.Printf("Initializing on default directory: %v.\n", red.Sprint(dir))
+				}
+
+				path, err := todos.Init(dir)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(0)
+				}
+
+				fmt.Printf("Created today's to-do file: %s\n", green.Sprint(path))
+				return nil
+			},
+		},
+	}
+
 	app.Action = func(c *cli.Context) error {
-		todos.PrintTodos(path)
 		return nil
 	}
 	_ = app.Run(os.Args)
