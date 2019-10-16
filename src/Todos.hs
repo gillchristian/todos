da0d8ff0eca6a11b8d02f12a0d3a5cb0ba53bec1
gillchristian <gillchristiang@gmail.com>
2019-10-16 20:40:29 +0200

    feat(cli): Add `td last` command
    Also a few small fixes and improvements

--- src/Todos.hs
+++ src/Todos.hs
@@ -5,6 +5,7 @@ where
 
 import Control.Monad (void)
 import qualified Data.Dates as Dates
+import qualified Data.Dates.Formats as Dates
 import Data.Functor ((<&>))
 import qualified Data.List as List
 import qualified System.Directory as Dir
@@ -50,6 +51,9 @@ todoFileParser name = do
   eolOrEof
   TodoFile name todos <$> Parsec.many line
 
+parseFileName :: String -> Either Parsec.ParseError Dates.DateTime
+parseFileName = Dates.parseDateFormat "YYYY-MM-DD.txt"
+
 -- Types ---
 
 type Todo = String
@@ -59,13 +63,33 @@ type Done = String
 data TodoFile = TodoFile FilePath [Todo] [Done]
 
 instance Show TodoFile where
-  show (TodoFile _ todos dones) =
-    List.intercalate
-      "\n"
-      $ ["TODOS"]
-        ++ fmap ("- " ++) todos
-        ++ ["\nAccomplished"]
-        ++ fmap ("- " ++) dones
+  show = formatFile
+
+formatLine :: String -> String
+formatLine item = "- " ++ item ++ "\n"
+
+formatLineWithIndex :: (Int, String) -> String
+formatLineWithIndex (i, item) = "- " ++ show i ++ ": " ++ item ++ "\n"
+
+formatItems :: [String] -> String
+formatItems = concat . fmap formatLine
+
+formatItemsWithIndex :: [String] -> String
+formatItemsWithIndex = concat . fmap formatLineWithIndex . List.zip [1 ..]
+
+formatFile :: TodoFile -> String
+formatFile (TodoFile _ todos dones) =
+  "TODOS\n"
+    ++ formatItems todos
+    ++ "\nAccomplished\n"
+    ++ formatItems dones
+
+formatFileWithIndex :: TodoFile -> String
+formatFileWithIndex (TodoFile _ todos dones) =
+  "TODOS\n"
+    ++ formatItemsWithIndex todos
+    ++ "\nAccomplished\n"
+    ++ formatItemsWithIndex dones
 
 -- Paths ---
 
@@ -90,15 +114,21 @@ formatFileName d =
     ++ (printf "%02d" $ Dates.day d)
     ++ ".txt"
 
--- import qualified Data.Dates.Formats as Dates
--- parseFileName :: String -> Either Parsec.ParseError Dates.DateTime
--- parseFileName = Dates.parseDateFormat "YYYY-MM-DD.txt"
-
 path :: TodoFile -> FilePath
 path (TodoFile name _ _) = name
 
 addTodo :: String -> TodoFile -> TodoFile
 addTodo todo (TodoFile name todos dones) = TodoFile name (todos ++ [todo]) dones
+
+fromLastTodoFile :: TodoFile -> TodoFile
+fromLastTodoFile (TodoFile name todos _) = TodoFile name todos []
+
+yyyyMmDd :: Dates.DateTime -> String
+yyyyMmDd d =
+  (show $ Dates.year d) ++ "/"
+    ++ (printf "%02d" $ Dates.month d)
+    ++ "/"
+    ++ (printf "%02d" $ Dates.day d)
 
 -- TodoFile IO ---
 
@@ -119,12 +149,27 @@ readLastFile ::
 readLastFile basePath prevDayName todayName
   | todayName == prevDayName =
     Parsec.parseFromFile (todoFileParser todayName) $ basePath // todayName
-  | otherwise =
-    Parsec.parseFromFile (todoFileParser prevDayName) $ basePath // prevDayName
+  | otherwise = do
+    let fullPath = basePath // prevDayName
+    eitherFile <- Parsec.parseFromFile (todoFileParser todayName) fullPath
+    pure $ fromLastTodoFile <$> eitherFile
+
+prevFile ::
+  FilePath -> FilePath -> [FilePath] -> IO (Either Parsec.ParseError TodoFile)
+prevFile _ todayName [] =
+  pure $ Right $ TodoFile todayName [] []
+prevFile basePath todayName (lastFileName : rest) =
+  if todayName == lastFileName
+    then prevFile basePath todayName rest
+    else Parsec.parseFromFile (todoFileParser lastFileName) fullPath
+  where
+    fullPath = basePath // lastFileName
 
 -- CLI ---
 
 handleCommands :: FilePath -> [FilePath] -> [String] -> IO ()
+-- Adds a todo to today's file (creates it if it doesn't exist)
+--   $ td add [word...]
 handleCommands basePath todoFiles ("add" : todo) = do
   let newTodo = List.intercalate " " todo
   eitherFile <- todayFile basePath todoFiles
@@ -132,15 +177,27 @@ handleCommands basePath todoFiles ("add" : todo) = do
     Right file -> pure file
     Left err -> Exit.die $ show err
   saveTodoFile basePath file
-  print file
+  putStr $ formatFileWithIndex file
+-- Prints the last file (that is not today's one)
+--   $ td last
+handleCommands basePath todoFiles ("last" : _) = do
+  todayName <- formatFileName <$> Dates.getCurrentDateTime
+  eitherPrevFile <- prevFile basePath todayName todoFiles
+  prevF <- case eitherPrevFile of
+    Right file -> pure file
+    Left err -> Exit.die $ show err
+  case parseFileName $ path prevF of
+    Right date -> putStrLn $ "td file: " ++ yyyyMmDd date ++ "\n"
+    Left err -> printStderr err
+  putStr $ formatFileWithIndex prevF
+-- Lists today's file (creates it if it doesn't exist)
+--   $ td
 handleCommands basePath todoFiles _ = do
   eitherFile <- todayFile basePath todoFiles
   case eitherFile of
     Right file -> do
-      -- TODO: only write if it's a new file?
-      saveTodoFile basePath file
-      print file
-    Left err -> printStderr err
+      saveTodoFile basePath file -- TODO: only write if it's a new file?
+      putStr $ formatFileWithIndex file
 
 runCli :: IO ()
 runCli = do
